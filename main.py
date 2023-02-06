0bdbae1e5bdfc3a738c68ca283293ef670d50e16
EGAProg <gameswithiy@gmail.com>
2023-02-06 13:04:13 +0300

    Changed IF Else to Match Case

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import telebot
 bot = telebot.TeleBot('6073497379:AAE0Qaq-GyLY1XMKnxGxjxcwXxHkxAAwAUk')
 
 @bot.message_handler(commands=['start'])
+
 def start(message):
-    mess = f'гей, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
+    mess = f'OPPYM@LenovoLaptop, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
+
+'''
 @bot.message_handler()
+
 def get_user_message(message):
     if message.text == 'привет':
         bot.send_message(message.chat.id, "ты гей", parse_mode='html')
@@ -23,7 +27,24 @@ def get_user_message(message):
         bot.send_message(message.chat.id, 'https://vk.com/away.php?to=https%3A%2F%2Fvas3k.ru%2Fblog%2Fblockchain%2F&cc_key=')
     else :
         bot.send_message(message.chat.id, 'я тебя не понимаю', parse_mode='html')
-
+'''
+@bot.message_handler()
+def get_user_message(message):
+    match (message.text) :
+        case ["привет"]:
+            bot.send_message(message.chat.id, "ты гей", parse_mode='html')
+        case ["id"]:
+            bot.send_message(message.chat.id, f"твой id: {message.from_user.id}", parse_mode='html')
+        case ["photo"]:
+            photo = open('test.jpg', 'rb')
+            bot.send_photo(message.chat.id, photo)
+        case ["мох"]:
+            audio = open('mox.mp3', 'rb')
+            bot.send_audio(message.chat.id, audio)
+        case ["Авдеева"]:
+            bot.send_message(message.chat.id, 'https://vk.com/away.php?to=https%3A%2F%2Fvas3k.ru%2Fblog%2Fblockchain%2F&cc_key=')
+        case _ :
+            bot.send_message(message.chat.id, 'я тебя не понимаю', parse_mode='html')
 
 
 
