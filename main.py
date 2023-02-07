34e625cef65bd666ef1fb8f0133b826ce0208899
EGAProg <gameswithiy@gmail.com>
2023-02-07 08:36:45 +0300

    Fist iteration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot('6073497379:AAE0Qaq-GyLY1XMKnxGxjxcwXxHkxAAwAUk')
+bot = telebot.TeleBot('6141289418:AAF3T3gS9gCuXMmVsf7F9NbIhoGA7BNlpkc')
 
 @bot.message_handler(commands=['start'])
 
@@ -8,34 +8,25 @@ def start(message):
     mess = f'OPPYM@LenovoLaptop, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
-'''
+# 
+
 @bot.message_handler()
 def get_user_message(message):
-    if message.text == 'привет':
-        bot.send_message(message.chat.id, "ты гей", parse_mode='html')
-    elif message.text == 'id':
-        bot.send_message(message.chat.id, f"твой id: {message.from_user.id}", parse_mode='html')
-    elif message.text == "photo":
-        photo = open('test.jpg', 'rb')
-        bot.send_photo(message.chat.id, photo)
-    elif message.text == 'мох':
-        audio = open('mox.mp3', 'rb')
-        bot.send_audio(message.chat.id, audio)
-    elif message.text == 'Авдеева':
-        bot.send_message(message.chat.id, 'https://vk.com/away.php?to=https%3A%2F%2Fvas3k.ru%2Fblog%2Fblockchain%2F&cc_key=')
-    else :
-        bot.send_message(message.chat.id, 'я тебя не понимаю', parse_mode='html')
-'''
-@bot.message_handler()
-def get_user_message(message):
-    match (message.text) :
+    match (message.text):
         case "привет":
             bot.send_message(message.chat.id, "ты гей", parse_mode='html')
         case "id":
             bot.send_message(message.chat.id, f"твой id: {message.from_user.id}", parse_mode='html')
         case "photo":
-            photo = open('test.jpg', 'rb')
-            bot.send_photo(message.chat.id, photo)
+            print("Chose type: vilance or genshin?")
+            if message.text == "vilance":
+                photo = open("Rodrigo.png", "rb")
+                bot.send_photo(message.chat.id, photo)
+            if message.text == "genshin":
+                photo = open("test.jpg", "rb")
+                bot.send_photo(message.chat.id, photo)
+            else:
+                print("What?")
         case "мох":
             audio = open('mox.mp3', 'rb')
             bot.send_audio(message.chat.id, audio)
@@ -43,7 +34,5 @@ def get_user_message(message):
             bot.send_message(message.chat.id, 'https://vk.com/away.php?to=https%3A%2F%2Fvas3k.ru%2Fblog%2Fblockchain%2F&cc_key=')
         case _ :
             bot.send_message(message.chat.id, 'я тебя не понимаю', parse_mode='html')
-
-
 
 bot.polling(none_stop=True)
